ddf78d92a172e550a4b8abe46ccf49e395071af0
Andrey Smirnov <37780231+Adisey@users.noreply.github.com>
2018-03-28 01:15:17 +0300

    +++

--- javascript.js
+++ javascript.js
@@ -514,7 +514,7 @@ dw(today);
 
 var start = new Date; // засекли время
 // что-то сделать
-for (var i = 0; i < 10000000; i++) {
+for (var i = 0; i < 1000000; i++) {
   var doSomething = i * i * i;
 }
 var end = new Date; // конец измерения
